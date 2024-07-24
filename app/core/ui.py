dd167e09b75d1a96004137232661cb5665b03a2e
Lawrence <lawrence@cinnamon.is>
2024-07-24 20:32:54 +0700

    refactor: ui

--- app/core/ui.py
+++ app/core/ui.py
@@ -1,20 +1,29 @@
-from discord import Embed, SelectMenu
-from discord import User, Member
+from typing import Final
 
-EMOJI: list[str]= [":green_heart:", ":yellow_heart:", ":blue_heart:", ":purple_heart:", ":heart:"]
+from discord import Embed, Member, SelectMenu, User
+
+EMOJI: Final[list[str]] = [
+    ":green_heart:",
+    ":yellow_heart:",
+    ":blue_heart:",
+    ":purple_heart:",
+    ":heart:",
+]
+
 
 def form_embed(user: User | Member) -> Embed:
     """Form the embed to show the user their guess' details."""
-    embed = Embed(title= f"{user.name}'s Wordle Guess")
+    embed = Embed(title=f"{user.name}'s Wordle Guess")
 
-    guesses = ... #list of guesses
+    guesses: list[str] = []
     for idx, guess in enumerate(guesses):
         embed.add_field(
-            name= f"Guess #{idx + 1}",
-            value= (f"")
-            )
+            name=f"Guess #{idx + 1}",
+            value=guess,
+        )
 
     return embed
+
 
 def form_select_menu() -> SelectMenu:
     """Form the SelectMenu for the user to choose length of word."""
